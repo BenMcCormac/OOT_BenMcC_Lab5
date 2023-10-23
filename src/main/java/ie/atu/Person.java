f302a9219aa75e7e8203d37674f29e2c0010b8a4
BenMcCormack <g00416547@atu.ie>
2023-10-23 09:27:17 +0100

    Added function to class 'Person'

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -5,4 +5,17 @@ public class Person
     String firstName;
     String lastName;
     int age;
+
+    //Constructor
+    public Person(String firstName, String lastName, int age)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public void displayInfo()
+    {
+        System.out.println("Name: " + firstName + "" + lastName + ", Age: " + age);
+    }
 }
